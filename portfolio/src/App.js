cd74e0a43a63fd98589a5cc2af63f27ee849885e
Steve Olson <steve.olson357@gmail.com>
2025-07-12 19:15:55 -0700

    testing functionality for publishing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,16 @@
 // From default installation
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
 // Manual imports
-import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/bytebank" element={<HomePage />} />
       </Routes>
     </Router>
   );
--- portfolio/src/pages/Home/HomePage.jsx
+++ portfolio/src/pages/Home/HomePage.jsx
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Portfolio Home</title>
-</head>
-<body>
-    <h1>Home page of portfolio here</h1>
-</body>
-</html>
+import React from "react";
+// import styles from "./GroupSettingsPage.module.css";
+
+function HomePage() {
+
+      return (
+        <div>
+            <h1>Home page of portfolio here</h1>
+        </div>
+      );
+    };
+export default HomePage;
