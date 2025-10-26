dea6973cdb0b4fc9ca4bd3af7e50536230c1a3e4
Steve Olson <steve.olson357@gmail.com>
2025-10-25 20:36:22 -0700

    added some content in projects page and created notes and screenshots for past projects

--- portfolio/src/pages/Projects/ProjectsPage.jsx
+++ portfolio/src/pages/Projects/ProjectsPage.jsx
@@ -10,8 +10,16 @@ function ProjectsPage() {
       return (
         <>
             <Header />
-            <div className={styles.container} id="page-container"> 
-
+            <div className="page-container"> {/* Start page-container */}
+                <div> {/* Start projectsPage style container */}
+                  <h1 className="title-container">Projects</h1>
+                    <p>
+                      Welcome to my Projects page! 
+                      Here, you'll find a selection of my work that showcases my skills and experience in software development. 
+                      Each project highlights different technologies and problem-solving approaches I've utilized. 
+                      Feel free to explore and reach out if you have any questions or would like to collaborate!
+                    </p>
+                  </div>  {/* End projectsPage style container */}
             </div> {/* End page-container */} 
             <Footer />
         </>
